1b7aa9d70d3997ed15ee53f880278e8b763609a6
Krzysztof Burkot <buryPL@gmail.com>
2017-04-01 10:43:52 +0200

    03_datatypes_done

--- js/03_datatypes.js
+++ js/03_datatypes.js
@@ -1,3 +1,45 @@
 //zmienne nazywamy tzw notacją cammelCase, tzn pierwsze słowo z małej litery, a każde następne z dużej, bez podkreśleń czy myślników
 'use strict';
 
+/*zmienne liczbowe*/
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+
+/*zmienne string*/
+var wyplataString = "5000";
+var premiaString = "2500";
+var calkowitaWyplataString;
+
+calkowitaWyplataString = wyplataString + premiaString;
+
+console.log(calkowitaWyplataString);
+
+/*zmienna boolean*/
+var czyJestSmog;
+czyJestSmog = true;
+
+if (czyJestSmog) {
+    console.log("Jest SMOG");
+}else{
+    console.log("Nie ma SMOGa");
+}
+
+/*zmienne null/undefined*/
+var niezdefiniowanaZmienna;
+var nullZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullZmienna);
+
+
+var imieStudenta = "Michał";
+console.log(imieStudenta.indexOf('c'));
+
+var stringusPodwojnyCudzyslow = "string '";
+var stringusPojedynczyCudzyslow =  'string "'
