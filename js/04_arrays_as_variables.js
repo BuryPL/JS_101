73dc70882a619996b5ce0c50e2b4e8180a670cd2
Krzysztof Burkot <buryPL@gmail.com>
2017-04-01 10:58:06 +0200

    04_arrays_as_variables

---
+++ js/04_arrays_as_variables.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*zmienne tablicowe*/
+var imiona = new Array();
+var imionaStudentow = new Array(9);
+var imionaChlopakow = new Array('Mateuch', 'Michał','Krzysiek','Sergio');
+
+imionaStudentow[2] = "Przemek";
+
+console.log(imiona);
+console.log(imionaStudentow);
+console.log(imionaChlopakow);
+
+var wiek = []; /*ten zapis jest rowny var wiek = new Array()*/
+var wiekStudentow = [20,25,28];
+
+console.log(wiek);
+console.log(wiekStudentow);
