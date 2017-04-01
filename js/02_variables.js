9893d702464523c018997ec9126f3d335d5105ea
Krzysztof Burkot <buryPL@gmail.com>
2017-04-01 09:39:24 +0200

    02_variables

---
+++ js/02_variables.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+var color = "czerwony";
+
+console.log(imie);
+console.log(color);
+console.log(wiek);
+
+wiek=21;
+
+console.log(wiek);
